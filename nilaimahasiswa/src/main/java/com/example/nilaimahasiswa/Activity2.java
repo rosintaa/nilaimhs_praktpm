6cbff482a9d89e26990edc57688e98192ebfe345
Rosinta Anggraini <57403869+rosintaa@users.noreply.github.com>
2021-04-05 16:52:22 +0700

    Add files via upload

---
+++ nilaimahasiswa/src/main/java/com/example/nilaimahasiswa/Activity2.java
@@ -0,0 +1,53 @@
+package com.example.nilaimahasiswa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Activity2 extends AppCompatActivity {
+    TextView bNama, bNIM, bNilai;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity2);
+
+        bNama = findViewById(R.id.bnama);
+        bNIM = findViewById(R.id.bnim);
+        bNilai = findViewById(R.id.bnilai);
+
+        String nama, nim, huruf;
+        double nilaihuruf;
+
+        nama = getIntent().getStringExtra("nama");
+        bNama.setText(nama);
+
+        nim = getIntent().getStringExtra("nim");
+        bNIM.setText(nim);
+
+        huruf = getIntent().getStringExtra("nilai");
+        nilaihuruf = Double.parseDouble(huruf);
+        if (nilaihuruf > 3.66 && nilaihuruf <= 4) {
+            bNilai.setText("A");
+        } else if (nilaihuruf > 3.33 && nilaihuruf <= 3.66) {
+            bNilai.setText("A-");
+        } else if (nilaihuruf > 3 && nilaihuruf <= 3.33) {
+            bNilai.setText("B+");
+        } else if (nilaihuruf > 2.66 && nilaihuruf <= 3) {
+            bNilai.setText("B");
+        } else if (nilaihuruf > 2.33 && nilaihuruf <= 2.66) {
+            bNilai.setText("B-");
+        } else if (nilaihuruf > 2 && nilaihuruf <= 2.33) {
+            bNilai.setText("C+");
+        } else if (nilaihuruf > 1.66 && nilaihuruf <= 2) {
+            bNilai.setText("C");
+        } else if (nilaihuruf > 1.33 && nilaihuruf <= 1.66) {
+            bNilai.setText("C-");
+        } else if (nilaihuruf > 1 && nilaihuruf <= 1.33) {
+            bNilai.setText("D+");
+        } else if (nilaihuruf == 1) {
+            bNilai.setText("D");
+        }
+    }
+}
---
+++ nilaimahasiswa/src/main/java/com/example/nilaimahasiswa/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.nilaimahasiswa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button submit;
+    private EditText eNama, eNIM, eNilai;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        submit = findViewById(R.id.submit);
+        eNama = findViewById(R.id.nama);
+        eNIM = findViewById(R.id.nim);
+        eNilai = findViewById(R.id.nilai);
+    }
+
+    public void setSubmit(View view) {
+        try {
+            String aNama = eNama.getText().toString();
+            String aNIM = eNIM.getText().toString();
+            String aNilai = eNilai.getText().toString();
+            Intent intent = new Intent(MainActivity.this, Activity2.class);
+            intent.putExtra("nama", aNama);
+            intent.putExtra("nim", aNIM);
+            intent.putExtra("nilai", aNilai);
+            startActivity(intent);
+        } catch (NumberFormatException e) {
+            Toast.makeText(getApplicationContext(), "Field kosong", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+}
